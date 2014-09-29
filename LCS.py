a83f381204c65cee6783725bb20f09fe60720397
tkyf <tkyf@users.noreply.github.com>
2014-09-29 23:54:52 +0900

    Add LCS base.

---
+++ LCS.py
@@ -0,0 +1,22 @@
+#!/usr/env python
+# coding:utf-8
+
+
+def lsc(str1, str2):
+    pass
+
+
+def fill_in():
+    pass
+
+
+def trace_back():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
