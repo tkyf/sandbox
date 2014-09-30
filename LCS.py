d409028da4deaf1a1bcc579e57311e6a84ce0462
tkyf <tkyf@users.noreply.github.com>
2014-10-01 00:22:49 +0900

    Implement algorithm of recursive longest common subsequence.

--- LCS.py
+++ LCS.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+from __future__ import print_function
+import sys
 
-def lsc(str1, str2):
+
+def lcs(str1, str2):
     pass
 
 
@@ -14,9 +17,33 @@ def trace_back():
     pass
 
 
+def recursive_lcs(str1, str2):
+    if str1 == '' or str2 == '':
+        return ''
+    else:
+        c1, str1_dash = str1[-1], str1[:-1]
+        c2, str2_dash = str2[-1], str2[:-1]
+        L1 = recursive_lcs(str1_dash, str2)
+        L2 = recursive_lcs(str1, str2_dash)
+        L3 = recursive_lcs(str1_dash, str2_dash)
+        if c1 == c2:
+            return longest_string(L1, L2, L3 + c1)
+        else:
+            return longest_string(L1, L2, L3)
+
+
+def longest_string(*strings):
+    return sorted(strings)[-1]
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        print('Usage: $ python LCS.py string1 string2')
+        return 1
+
+    print(recursive_lcs(sys.argv[1], sys.argv[2]))
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
