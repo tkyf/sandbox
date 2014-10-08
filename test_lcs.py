6ffca09a05d8d57cc3b79d76c2ce498da2a85831
tkyf <tkyf@users.noreply.github.com>
2014-10-08 23:01:23 +0900

    Add test cases of recursive method.

--- test_lcs.py
+++ test_lcs.py
@@ -17,5 +17,12 @@ class TestLCS(unittest.TestCase):
         self.assertEqual(lcs.lcs("A", "D"), "")
         self.assertEqual(lcs.lcs("aa", "a"), "a")
 
+    def test_recursive_lcs(self):
+        self.assertEqual(lcs.recursive_lcs("ABCDE", "ABDCE"), "ABDE")
+        self.assertEqual(lcs.recursive_lcs("", "ABDCE"), "")
+        self.assertEqual(lcs.recursive_lcs("ABCDE", ""), "")
+        self.assertEqual(lcs.recursive_lcs("A", "D"), "")
+        self.assertEqual(lcs.recursive_lcs("aa", "a"), "a")
+
 if __name__ == '__main__':
     unittest.main()
